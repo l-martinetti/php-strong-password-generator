daa71afcf42315ba502cc7e1b7a226557bb55e59
l-martinetti <luca.martinetti@outlook.it>
2025-03-26 15:44:11 +0100

    feat: add functions file and moved logic to that file

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php
 
-    function get_password() {
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $randomString = '';
-
-        if( isset($_GET['password_length']) && is_numeric($_GET['password_length'])){
-
-            for ($i = 0; $i < $_GET['password_length']; $i++) {
-                $index = random_int(0, strlen($characters) - 1);
-                $randomString .= $characters[$index];
-            }
-        }
-
-        return $randomString;
-    }
+    require_once "./functions.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
